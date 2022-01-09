a3d362aff790a906f4da615d4e4734350ba2e631
Dinesh <dinesh.redmipro@gmail.com>
2022-01-09 21:22:30 +0530

    Create arith_opn.py
    python script to perform basic arithmatic operations

---
+++ arith_opn.py
@@ -0,0 +1,22 @@
+print("Please Enter the value ")
+x=input("Enter first number ")
+y=input("Enter second number ")
+def sum(x,y):
+	print("Sum of two number ",(int(x)+int(y)))
+def sub(x,y):
+	print("Sum of two number ",(int(x)-int(y)))
+def mult(x,y):
+	print("Sum of two number ",(int(x)*int(y)))
+
+if __name__ == "__main__":
+	z=input("Enter operation : ")
+	if "sum"==z:
+		sum(x,y)
+	elif "sub"==z:
+		sub(x,y)
+	elif "mult"==z:
+		mult(x,y)
+	else:
+		print("No Operation has specified  ")
+
+	
